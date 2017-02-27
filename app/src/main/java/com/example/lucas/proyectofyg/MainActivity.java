34cd2f4cb1546b92f2f7a71a9f15e057f745e092
gabrielmamani <mamanigabriel2014@gmail.com>
2017-02-27 09:00:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lucas/proyectofyg/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lucas.proyectofyg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //prueba3
+        setContentView(R.layout.activity_main);
+    }
+}
